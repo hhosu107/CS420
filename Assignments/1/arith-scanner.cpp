ad83ef4b6da06cce721dfc0083779fe935e13062
Chansu Park <hhosu107@gmail.com>
2018-09-25 01:14:40 +0900

    Add scanner

---
+++ Assignments/1/arith-scanner.cpp
@@ -0,0 +1,165 @@
+#include<cstdlib>
+#include<iostream>
+#include<sstream>
+#include<cctype>
+#include<cstring>
+#include<string>
+#include<cassert>
+#include<cstdio>
+
+#include "arith-scanner.hpp"
+using namespace std;
+
+#define TOKEN_LEN 12
+
+char enumTokenName[][TOKEN_LEN] = {
+  "tNumber",
+  "tID",
+  "tPlusMinus",
+  "tMulDiv",
+  "tEOF",
+  "tError",
+  "tUndefined"
+};
+
+Token::Token(enumToken type, const stringvalue){
+  _type = type;
+  _value = value;
+}
+
+Token::Token(const Token &token){
+  _type = token.getTokenType();
+  _value = token.getTokenValue();
+}
+
+Token::Token(const Token *token){
+  _type = token->getTokenType();
+  _value = token->getTokenValue();
+}
+
+static const string Token::tokenName(enumToken type){
+  return string(enumTokenName[type]);
+}
+
+const string Token::getTokenName(){
+  return string(enumTokenName[getTokenType()]);
+}
+
+ostream& Token:print(ostream &out) const{
+  out << getTokenValue();
+  return out;
+}
+
+Scanner::Scanner(istream *in){
+  _in = in;
+  _delete = false;
+  _okay = true;
+  _token = NULL;
+}
+
+Scanner::Scanner(string in){
+  _in = new istringstream(in);
+  _delete = false;
+  _okay = true;
+  _token = NULL;
+}
+
+Scanner::~Scanner(){
+  if(_token != NULL) delete _token;
+  if(_delete) delete _in;
+}
+
+Token Scanner::getNext(){
+  Token result(_token); // prepare the next token: note that this initializer copies the content, so changing _token doesn't affect.
+
+  enumToken type = _token->getTokenType();
+  _okay = !(type == tError); // check the current token: tError or not
+
+  nextToken(); // and get the next token and return itself
+  return result;
+}
+
+Token Scanner::peekNext() const{
+  return Token(_token);
+}
+
+void Scanner::nextToken(){
+  if(_token != NULL) delete _token;
+  _token = scan(); // fill in the next token
+}
+
+Token* Scanner::scan(){
+  // Rule-based scan
+}
+
+Token* Scanner::newToken(enumToken token, const string value){
+  return new Token(token, value);
+}
+
+Token* Scanner::scan(){
+  enumToken token;
+  string value;
+  char c;
+
+  while(_in->good() && isWhite(_in->peek())){
+    getNextChar();
+  } // eliminate whitespaces. Since it peeks, the next getNextChar() will return the valid character.
+  // stringstream states: good / eof / fail / bad. Any false value of good() except eof means an error on reading character on the stringstream.
+  if(_in->eof()) return newToken(tEOF); // EOF token
+  if(!_in->good()) return newToken(tError); // error on scanning
+
+  c = getNextChar();
+  
+  // initialize members of token
+  value = c;
+  token = tUndefined;
+
+  // tokenize by rule: digit / alphabet / others
+  switch(c){
+    case '+':
+    case '-':
+      token = tPlusMinus;
+      break;
+    case '*':
+    case '/':
+      token = tMulDiv;
+      break;
+    default:
+      if(isDigit(c)){
+        token = tNumber;
+        // peek until characters other than digit found
+        c = _in->peek();
+        while(!isDigit(c)){
+          value += getNextChar();
+          c = _in->peek();
+        }
+      }
+      else if(isAlphabet(c)){
+        token = tID;
+      }
+      else{ // since whitespaces are already detected before switch starts, other characters are invalid.
+        value = "";
+        _okay = false; 
+      }
+      break;
+
+  }
+
+  return new Token(token, value);
+}
+
+char Scanner::getNextChar(){
+  return _in->get();
+}
+
+bool isDigit(char c){
+  return (c >= '0' && c <= '9');
+}
+
+bool isAlphabet(char c){
+  return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+}
+
+bool isWhite(char c){
+  return (c == ' ' || c == '\n' || c == '\t' || c == 'r');
+}
---
+++ Assignments/1/arith-scanner.hpp
@@ -0,0 +1,68 @@
+#ifndef __SCANNER_HPP__
+#define __SCANNER_HPP__
+
+#include<iostream>
+#include<iomanip>
+#include<map>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+#include<string>
+#include<cstdlib>
+
+using namespace std;
+
+enum enumToken{
+  tNumber = 0, // Integer := digit { digit }
+  tID, // Identifier := alphabet
+  tPlusMinus, // PlusMinus := '+' | '-'
+  tMulDiv, // MulDiv := '*' | '/'
+  tEOF, // Indicating the end of the line.
+  tError, // any error on the scanner: character is not a value included in digit or alphabet.
+  tUndefined // Initial token.
+};
+
+class Token{
+  friend class Scanner;
+  private:
+    enumToken _type; // contains the type of the token
+    string _value; // contains the value of the token
+  public:
+    Token(enumToken type, const stringvalue="");
+    Token(const Token &token); // copy token from the reference
+    Token(const Token *token); // copy token from the pointer
+    static const string tokenName(enumToken type); // return the name of the token type for given type
+    const string getTokenName() const; // return the name of the token type of the given token
+    enumToken getTokenType() const{
+      return _type;
+    } // return the enumerated type of the token
+    string getTokenValue() const{
+      return _value;
+    } // return the value of the token
+    ostream& print(ostream &out) const; // print the value of the token to the output stream
+}
+
+class Scanner{
+  private:
+    istream * _in; // input stream
+    bool _delete; // delete inpus stream when destruct
+    bool _okay; // Does scanner has no error or not?
+    CToken * _token; // Contains the next token in the input stream 
+    // vector<Token*> _token // vector of Token pointers from the input stream
+  public:
+    Scanner(istream *in); // Use stream operators like peek
+    Scanner(string in); // Change string to string stream
+    ~Scanner(); // destruct
+
+    CToken getNext(); // return and remove the next token
+    CToken peekNext() const; // peek the next token without removing it
+    book okay() const { return _okay }; // Check the status of the scanner
+    void nextToken(); // scan the next token
+    Token* newToken(enumToken type, const string token); // initializing token by given information
+    Token* scan(); // scan the input stream and return the next token
+    char getNextChar(); // next character in the input stream
+    bool isDigit(char c) const; // check if a character is a digit = ('0', ..., '9')
+    bool isAlphabet(char c) const; // check if a character is an alphabet = ('A', ..., 'Z', 'a', ..., 'z')
+    bool isWhite(char c) const; // check if a character is a whitespace character = (' ', '\n', 't')
+
+#endif
